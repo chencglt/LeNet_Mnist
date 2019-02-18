e5f2d009c07314229ce9899b8478215825042321
chencglt <chencglt@gmail.com>
2019-02-18 17:39:57 +0800

    add batch_norm

--- LeNet.py
+++ LeNet.py
@@ -4,10 +4,11 @@ import config as cfg
 slim = tf.contrib.slim
 
 class LeNet:
-    def network_fn(self, input, REUSE=False):
+    def network_fn(self, input, is_training):
+        reuse = False if is_training else True
         net = tf.image.resize_images(input, [cfg.LENET_INPUT_W, cfg.LENET_INPUT_H], method=tf.image.ResizeMethod.BILINEAR)
-        with slim.arg_scope([slim.conv2d], stride=1, padding='VALID'):
-            with tf.variable_scope('lenet', reuse=REUSE):
+        with slim.arg_scope([slim.conv2d], stride=1, normalizer_fn=slim.batch_norm, padding='VALID'):
+            with tf.variable_scope('lenet', reuse=reuse):
                 with tf.variable_scope('conv1'):
                     net = slim.conv2d(net, 6, [5, 5])
                     net = slim.avg_pool2d(net, 2, 2)
@@ -19,5 +20,6 @@ class LeNet:
                 with tf.variable_scope('full_conn'):
                     net = slim.fully_connected(net, 120)
                     net = slim.fully_connected(net, 84)
+                    #net = slim.dropout(net, DROPOUT_RATIO, is_training=is_training, scope='dropout8')
                     net = slim.fully_connected(net, cfg.LABEL_SIZE)
                 return net
--- train.py
+++ train.py
@@ -15,33 +15,39 @@ def inference(input_x, input_y):
 
     #regularizer = tf.contrib.layers.l2_regularizer(REGULARIZATION_RATE)
     le_network = LeNet()
-    y = le_network.network_fn(x)
-    #y_test = LeNet(x_t, True)
-    correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
-    accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+    result_train = le_network.network_fn(x, is_training=True)
+    result_test = le_network.network_fn(x, is_training=False)
+
+    #correct_train = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+    correct_test = tf.equal(tf.argmax(result_test, 1), tf.argmax(y_, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_test, "float"))
 
     # loss, learning rate, moving_average
     #variable_averages = tf.train.ExponentialMovingAverage(MOVING_AVERAGE_DECAY, global_step)
     #variables_averages_op = variable_averages.apply(tf.trainable_variables())
-    cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=y, labels=y_)
+    cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=result_train, labels=y_)
     cross_entropy_mean = tf.reduce_mean(cross_entropy)
-    loss = cross_entropy_mean# + tf.add_n(tf.get_collection('losses'))
+    loss = cross_entropy_mean # + tf.add_n(tf.get_collection('losses'))
     learning_rate = tf.train.exponential_decay(
         cfg.LEARNING_RATE_BASE,
         global_step,
         mnist.train.num_examples / cfg.BATCH_SIZE, cfg.LEARNING_RATE_DECAY,
         staircase=True)
 
-    train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss, global_step=global_step)
-    train_op = train_step
+    # update_ops & tf.control_dependencies are needed when introducing batch_norm
+    update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS) 
+    with tf.control_dependencies(update_ops): 
+        train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss, global_step=global_step)
+        train_op = train_step
+
     return train_op, loss, global_step, accuracy
 
 def main_loop():
     test_accuracy = []
     #saver = tf.train.Saver()
     with tf.Session() as sess:
-        x = tf.placeholder(tf.float32, [None, cfg.IMAGE_W, cfg.IMAGE_W, 1], name='x-input')
-        y_ = tf.placeholder(tf.float32, [None, cfg.LABEL_SIZE], name='y-input')
+        x = tf.placeholder(tf.float32, [None, cfg.IMAGE_W, cfg.IMAGE_H, 1], name='x_input')
+        y_ = tf.placeholder(tf.float32, [None, cfg.LABEL_SIZE], name='y_label')
         train_op, loss, global_step, infer_accuracy = inference(x, y_)
         tf.global_variables_initializer().run()
         for i in range(cfg.TRAINING_STEPS):
